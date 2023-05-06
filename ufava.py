8aeebc9429bc60b982119cfa2a1cc369d6e2675f
Keith Pine <keith.pine@gmail.com>
2023-05-06 10:30:13 -0700

    Add log level env var

--- ufava.py
+++ ufava.py
@@ -11,11 +11,14 @@ if not env_filenames:
 
 app.config["BEANCOUNT_FILES"] = env_filenames.split()
 
+# 'critical', 'error', 'warning', 'info', 'debug', 'trace'.
+log_level = os.environ.get("LOG_LEVEL", "info")
+
 uvicorn.run(
     "fava.application:app",
     host="0.0.0.0",
     port=5000,
-    log_level="info",
+    log_level=log_level,
     interface="wsgi",
     proxy_headers=True,
 )
