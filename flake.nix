eda3a01580e2ad8576dfdce7f5ff3c1df3f63acd
Keith Pine <keith.pine@gmail.com>
2024-03-23 10:41:32 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,8 @@
     self,
     nixpkgs,
   }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    #supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    supportedSystems = ["x86_64-linux"];
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
@@ -17,21 +18,22 @@
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs;
-          [python311 virtualenv]
-          ++ (with pkgs.python311Packages; [
-            a2wsgi
-            pip
-            uvicorn
-          ])
-          ++ (with pkgs.python311Packages; [
-            (fava.overridePythonAttrs (oldAttrs: rec {
-              version = "1.27.1";
-              src = oldAttrs.src.override {
-                inherit version;
-                hash = "sha256-W/uxzk+/4tDVOL+nVUJfyBAE5sI9/pYq1zu42GCGjSk=";
-              };
-            }))
+          [python312] #virtualenv]
+          ++ (with pkgs.python312Packages; [
+            # a2wsgi
+            # pip
+            # uvicorn
+            fava
           ]);
+        # ++ (with pkgs.python312Packages; [
+        #   (fava.overridePythonAttrs (oldAttrs: rec {
+        #     version = "1.27.2";
+        #     src = oldAttrs.src.override {
+        #       inherit version;
+        #       hash = "sha256-W/uxzk+/4tDVOL+nVUJfyBAE5sI9/pYq1zu42GCGjSk=";
+        #     };
+        #   }))
+        # ]);
         shellHook = ''
           export BEANCOUNT_FILE="$PWD/example.beancount"
         '';
